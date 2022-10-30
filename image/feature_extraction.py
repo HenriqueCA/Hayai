6d826479a07c3e4b74133069c5c6feac9a4e2b99
Henrique Castro Arriel <henrique.arriel@ccc.ufcg.edu.br>
2022-10-30 16:44:22 -0300

    docs and requirements

--- image/feature_extraction.py
+++ image/feature_extraction.py
@@ -5,6 +5,8 @@ import numpy as np
 from scipy.spatial import distance
 
 class FeatureModel:
+    """ Simple class to compare images similarities with EfficientNet-Lite (B0) model feature extraction"""
+
     def __init__(self):
         self.IMAGE_SHAPE = (224,224)
         model_url = "https://tfhub.dev/tensorflow/efficientnet/lite0/feature-vector/2"
@@ -12,6 +14,15 @@ class FeatureModel:
         self.model = tf.keras.Sequential([layer])
 
     def extract_features(self, file):
+        """
+        Extract features from image file using NN model.
+
+        Args:
+            file (str): path to image file to be extracted.
+
+        Returns:
+            float []: flattened features of image.
+        """
         file = Image.open(file).convert('L').resize(self.IMAGE_SHAPE)  #1
         file = np.stack((file,)*3, axis=-1)                       #2
         file = np.array(file)/255.0                               #3
@@ -21,7 +32,19 @@ class FeatureModel:
         flattended_features = features_np.flatten()
         return flattended_features
     
+        
     def compare_images(self, path_1, path_2, metric = 'cosine'):
+        """
+        Compare two images using features extracted by NN model given a metric.
+
+        Args:
+            path_1 (str): path to image compared.
+            path_2 (str): path to image compared.
+            metric (str): distance metric (scipy cdist) used to calculate between image features (default is cosine). 
+
+        Returns:
+            numpy.ndarray : distance between images.
+        """
         features_1 = self.extract_features(path_1)
         features_2 = self.extract_features(path_2)
         dis = distance.cdist([features_1], [features_2], metric)[0]
