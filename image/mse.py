21d2b4c0b852457d67beec515d0a54c848c9c58c
Henrique Castro Arriel <henrique.arriel@ccc.ufcg.edu.br>
2022-10-30 16:00:17 -0300

    added docs to mse

--- image/mse.py
+++ image/mse.py
@@ -2,6 +2,17 @@ import numpy as np
 import cv2
 
 def mse(img_1, img_2):
+   """
+   Calculates Mean Squared Error between two grayscale images with the same shape.
+
+   Args:
+      img_1 (cv2.Mat): image to calculate MSE
+      img_2 (cv2.Mat): image to calculate MSE
+
+   Returns:
+      float: MSE of the two images.
+   """
+   
    h, w = img_1.shape
    diff = cv2.subtract(img_1, img_2)
    err = np.sum(diff**2)
--- images/mse.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-
-def mse(img_1, img_2):
-   h, w = img_1.shape
-   diff = cv2.subtract(img_1, img_2)
-   err = np.sum(diff**2)
-   mse = err/float(h*w)
-   return mse
